34a64fb0a021cabcedd92f81d179658fd9aaa50b
kyungtackhong <rudxor94@ajou.ac.kr>
2015-11-03 13:04:45 +0900

    로그인&회원가입 액티비티

---
+++ cook/app/src/main/java/com/sap/team_f/cook/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.sap.team_f.cook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KyungTack on 2015-11-03.
+ */
+public class SignupActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signupactivity);
+    }
+}
--- cook/app/src/main/java/com/sap/team_f/cook/StartActivity.java
+++ cook/app/src/main/java/com/sap/team_f/cook/StartActivity.java
@@ -1,7 +1,10 @@
 package com.sap.team_f.cook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by KyungTack on 2015-10-29.
@@ -9,6 +12,18 @@ import android.os.Bundle;
 public class StartActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.startactivity);
+
+        Button signupBtn = (Button)findViewById(R.id.startSignupBtn);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

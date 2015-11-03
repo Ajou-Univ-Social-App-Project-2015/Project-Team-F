a45b54c3dfe92bb27e294ddfd5290de70a8fd286
kyungtackhong <rudxor94@ajou.ac.kr>
2015-11-03 12:22:17 +0900

    프로젝트파일

---
+++ cook/app/src/main/java/com/sap/team_f/cook/StartActivity.java
@@ -0,0 +1,14 @@
+package com.sap.team_f.cook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KyungTack on 2015-10-29.
+ */
+public class StartActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

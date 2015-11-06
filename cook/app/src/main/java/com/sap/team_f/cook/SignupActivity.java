4e2ab82b0eb7ccaab0ee8e3f2b240d3e1e069776
kyungtackhong <rudxor94@ajou.ac.kr>
2015-11-06 11:59:55 +0900

    로그인&회원가입
    서버연결

--- cook/app/src/main/java/com/sap/team_f/cook/SignupActivity.java
+++ cook/app/src/main/java/com/sap/team_f/cook/SignupActivity.java
@@ -2,6 +2,16 @@ package com.sap.team_f.cook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * Created by KyungTack on 2015-11-03.
@@ -9,7 +19,51 @@ import android.os.Bundle;
 public class SignupActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signupactivity);
+
+        Button signupBtn = (Button)findViewById(R.id.signupBtn);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText idText = (EditText)findViewById(R.id.signupIdText);
+                String id = idText.getText().toString();
+                EditText passText = (EditText)findViewById(R.id.signupPassText);
+                String pass = passText.getText().toString();
+                EditText repassText = (EditText)findViewById(R.id.signupRePassText);
+                String repass = repassText.getText().toString();
+                EditText nickText = (EditText)findViewById(R.id.signupNickText);
+                String nick = nickText.getText().toString();
+                Log.v("pass",pass);
+                Log.v("repass",repass);
+                if(repass.equals(pass))
+                {
+                    ParseUser user = new ParseUser();
+                    user.setUsername(id);
+                    user.setPassword(pass);
+
+                    user.put("Nickname",nick);
+                    Log.v("User",user.toString());
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(SignupActivity.this,"가입한 아이디로 로그인 해주세요",Toast.LENGTH_SHORT).show();
+                                finish();
+                                // Hooray! Let them use the app now.
+                            } else {
+                                Toast.makeText(SignupActivity.this,"이미 존재하는 아이디입니다.",Toast.LENGTH_SHORT).show();
+                                // Sign up didn't succeed. Look at the ParseException
+                                // to figure out what went wrong
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(SignupActivity.this,"비밀번호가 맞지 않습니다.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- cook/app/src/main/java/com/sap/team_f/cook/StartActivity.java
+++ cook/app/src/main/java/com/sap/team_f/cook/StartActivity.java
@@ -3,8 +3,17 @@ package com.sap.team_f.cook;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * Created by KyungTack on 2015-10-29.
@@ -12,10 +21,34 @@ import android.widget.Button;
 public class StartActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Parse.initialize(this, "tpbdNdyQ4hOngQCvtcMN3wzScHZGZXzWODMZvMbE", "olnEhtF4zxa1uk9YloYkA03vW60yuseqXVTM8oJU");
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.startactivity);
+
+        Button signinBtn = (Button)findViewById(R.id.signinBtn);
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText idText = (EditText)findViewById(R.id.startIdText);
+                String id = idText.getText().toString();
+                EditText passText = (EditText)findViewById(R.id.startPassText);
+                String pass = passText.getText().toString();
+                ParseUser.logInInBackground(id, pass, new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Toast.makeText(StartActivity.this, "로그인성공", Toast.LENGTH_SHORT).show();
+                            // Hooray! The user is logged in.
+                        } else {
+                            Toast.makeText(StartActivity.this, "아이디나 비밀번호가 맞지 않습니다.", Toast.LENGTH_SHORT).show();
+                            // Signup failed. Look at the ParseException to see what happened.
+                        }
+                    }
+                });
+            }
+        });
+
+
 
         Button signupBtn = (Button)findViewById(R.id.startSignupBtn);
         signupBtn.setOnClickListener(new View.OnClickListener() {
